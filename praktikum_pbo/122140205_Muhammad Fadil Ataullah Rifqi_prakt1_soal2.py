1583bb793b86eb8f5e78cd815f9117d866355a2b
Fadil Rifqi <108844635+FadilRifqi@users.noreply.github.com>
2024-02-27 18:32:11 +0700

    Delete praktikum_pbo directory

--- praktikum_pbo/122140205_Muhammad Fadil Ataullah Rifqi_prakt1_soal2.py
+++
@@ -1,9 +0,0 @@
-jari_jari = int(input("jari jari : "))
-
-if jari_jari <= 0:
-    print("jari-jari lingkaran tidak boleh negatif")
-else:
-    keliling = 2 * 3.14 * jari_jari
-    luas = 3.14 * jari_jari**2
-    print("Luas : ", luas)
-    print("keliling : ", keliling)
